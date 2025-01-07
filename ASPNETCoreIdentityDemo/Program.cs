cacccc53c5770d2835149d8f198c841decc32ca0
Roberto <roberto.labarca.github@outlook.com>
2025-01-07 17:15:14 -0500

    update

--- ASPNETCoreIdentityDemo/Program.cs
+++ ASPNETCoreIdentityDemo/Program.cs
@@ -1,7 +1,12 @@
+using ASPNETCoreIdentityDemo.Models;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
